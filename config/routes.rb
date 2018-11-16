af07edb25a8cf3554cf46194d5af6d47e10f977f
Balache404 <morgancuell@shaw.ca>
2018-11-16 17:07:22 +0000

    Series and characters routes added

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :figures, only: [:index, :show]
+  resources :series, only: [:index, :show]
+  resources :characters, only: [:index, :show]
 
   root to: 'figures#index'
   get 'figures/:id', to: 'figures#show'
+
+  get '/series', to: 'series#index'
+  get '/series/:id', to: 'series#show'
+
+  get '/character', to: 'characters#index'
+  get '/character/:id', to: 'characters#show'
 end

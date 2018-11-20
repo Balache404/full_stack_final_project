1388e72be29b32f6c02ee94ef19c153ee10888f6
Balache404 <morgancuell@shaw.ca>
2018-11-20 13:45:16 +0000

    Page table migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_172635) do
+ActiveRecord::Schema.define(version: 2018_11_20_134353) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -77,6 +77,13 @@ ActiveRecord::Schema.define(version: 2018_11_07_172635) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "provinces", force: :cascade do |t|
